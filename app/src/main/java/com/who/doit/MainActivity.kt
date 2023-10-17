fd23041fae07a508335ee3593d87ce229f9c25d8
dotnetbabu <ayush06092002@gmail.com>
2023-10-17 11:33:24 +0530

    Completed

--- app/src/main/java/com/who/doit/MainActivity.kt
+++ app/src/main/java/com/who/doit/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.who.doit
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -15,9 +19,12 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Divider
@@ -25,11 +32,16 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.who.doit.ui.theme.DoITTheme
@@ -53,6 +65,9 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun CreateToDo() {
+    val buttonStateClicked = remember {
+        mutableStateOf(false)
+    }
     Card(
         modifier = Modifier
             .fillMaxWidth()
@@ -69,6 +84,88 @@ fun CreateToDo() {
         ) {
             CreateimageProfile()
             Divider(thickness = 2.dp)
+            CreateprofileInfo()
+            Button(onClick = {
+                buttonStateClicked.value = !buttonStateClicked.value
+            }
+                ) {
+                Text(text = "To Do List")
+            }
+            if(buttonStateClicked.value){
+                Create()
+            }
+            else{
+                Box {}
+            }
+        }
+
+
+
+    }
+}
+
+@Composable
+private fun CreateprofileInfo() {
+    Column(modifier = Modifier.padding(5.dp)) {
+        Text(
+            text = "Ayush K.",
+            modifier = Modifier.padding(3.dp),
+            style = MaterialTheme.typography.headlineMedium,
+            color = MaterialTheme.colorScheme.primary
+        )
+        Text(
+            text = "Have a Good Day Ayush!!",
+            modifier = Modifier.padding(3.dp)
+        )
+        Text(
+            text = "Here are your tasks for today.",
+            modifier = Modifier.padding(3.dp),
+            style = MaterialTheme.typography.titleMedium
+        )
+    }
+}
+
+@Preview
+@Composable
+fun Create() {
+    Box(modifier = Modifier
+        .fillMaxWidth()
+        .fillMaxHeight()
+        .padding(5.dp)) {
+        Surface(modifier = Modifier
+            .padding(3.dp)
+            .fillMaxHeight()
+            .fillMaxWidth(),
+            shape = RoundedCornerShape(corner = CornerSize(6.dp)),
+            border = BorderStroke(width = 2.dp, color = Color.LightGray)
+        )
+        {
+            ToDo(data = listOf("ToDo 1", "ToDo 2", "ToDo 3", "ToDo 4"))
+        }
+    }
+}
+
+@Composable
+fun ToDo(data: List<String>) {
+    LazyColumn() {
+        items(data){item ->
+            Card(modifier = Modifier
+                .padding(13.dp)
+                .fillMaxWidth(),
+                shape = RectangleShape,
+                elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)){
+                Row(modifier = Modifier
+                    .padding(8.dp)
+                    .fillMaxWidth()
+                    .background(MaterialTheme.colorScheme.surface)
+                    .padding(7.dp)) {
+                    CreateimageProfile(modifier = Modifier.size(80.dp))
+                    Column(modifier = Modifier.padding(7.dp).align(alignment = Alignment.CenterVertically)) {
+                        Text(text = item, fontWeight = FontWeight.Bold)
+                        Text(text = "Your Todo", style = MaterialTheme.typography.bodyMedium)
+                    }
+                }
+            }
         }
     }
 }
@@ -87,13 +184,13 @@ private fun CreateimageProfile(modifier: Modifier = Modifier) {
         Image(
             painter = painterResource(id = R.drawable.profile_image),
             contentDescription = "Profile Picture",
-            modifier = Modifier.size(135.dp),
+            modifier = modifier.size(135.dp),
             contentScale = ContentScale.Crop
         )
     }
 }
 
-@Preview(showBackground = true)
+//@Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     DoITTheme {
